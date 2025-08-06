c9756d5e0f32eb2c97552ead937fc06a2e62426f
Nitya242005 <yashuqueen24@gmail.com>
2025-08-06 22:35:18 +0530

    Remove duplicate corsheaders entry

--- authsystem/authsystem/settings.py
+++ authsystem/authsystem/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
